2e0513ba2ae1c49b4641f79a68d7c9c77c163618
shiqian <shiqian@gmail.com>
2024-10-30 17:19:58 +0800

    react  todo list

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [taskList, setTaskList] = useState([]);
+  const [taskName, setTaskName] = useState("");
 
+  const addTaskList = () => {
+    if (taskName) {
+      setTaskList([...taskList, { id: Date.now(), name: taskName }]);
+      setTaskName("");
+    } else {
+      alert("请输入任务名称");
+    }
+  };
+  const deleteTask = (item) => {
+    let list = taskList.filter((i) => i.id !== item.id);
+    setTaskList([...list]);
+  };
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <input
+        type="text"
+        value={taskName}
+        onChange={(e) => setTaskName(e.target.value)}
+      />
+      <input type="button" value="添加任务" onClick={() => addTaskList()} />
+      <ul>
+        {taskList.map((i) => {
+          return (
+            <li key={i.id}>
+              {i.name}
+              <input
+                type="button"
+                value="删除任务"
+                onClick={() => deleteTask(i)}
+              />
+            </li>
+          );
+        })}
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
