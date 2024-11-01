3feae873bdd8a02ca78f76dd7fbf37f46b294250
shiqian <shiqian@gmail.com>
2024-11-01 15:27:49 +0800

    删除多余

--- react-demo/src/App.js
+++ react-demo/src/App.js
@@ -1,14 +1,7 @@
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        
-      </header>
+    <div>
+      hello word
     </div>
   );
 }
